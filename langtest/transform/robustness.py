116f8c7c6f099491a0f137b161b55aa4b46e33d8
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2025-07-30 20:38:54 +0530

    feat: add test for RandomizeOptions transformation

--- langtest/transform/robustness.py
+++ langtest/transform/robustness.py
@@ -2295,7 +2295,5 @@ class RandomizeOptions(BaseRobustness):
                 shuffle_sample.shuffle(pattern=split_pattern)
 
                 transformed_samples.append(shuffle_sample)
-            elif isinstance(sample, str):
-                transformed_samples.append(None)
 
         return transformed_samples
